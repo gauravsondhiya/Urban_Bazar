05be9f4390b7aa2ecf709ddc208afad2f91ec7b9
Gaurav Sondhiya <gauravsondhiya1503@gmail.com>
2024-08-11 13:34:15 +0530

    ui update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -39,46 +39,40 @@ const Home = () => {
   return (dataget.length == "") ? <Shimmer /> : (
     <>
       <Slider />
-      <h3 className="text-left text-2xl m-3 font-bold">Categories</h3>
-      <div className="flex border border-box border-black m-5 p-5 justify-around text-2xl font-bold underline underline-offset-1">
-          <button onClick={() => filterByCategory("all")}>All</button>
-          <button onClick={() => filterByCategory("beauty")}>Beauty</button>
-          <button onClick={() => filterByCategory("fragrances")}>
-            Fragrances
-          </button>
-          <button onClick={() => filterByCategory("furniture")}>
-            Furniture
-          </button>
-          <button onClick={() => filterByCategory("groceries")}>
-            Groceries
-          </button>
+      <h3 className="text-left text-2xl m-3 font-bold ">Categories</h3>
+      <div className="grid gap-2 max-sm:no-underline sm:grid-cols-5 sm:border border-black m-5 p-5 sm:justify-around text-2xl font-bold underline underline-offset-1">
+        <button className="max-sm:border border-box border-black hover:bg-red-400 rounded-lg"  onClick={() => filterByCategory("all")}>All</button>
+        <button className="max-sm:border border-box border-black hover:bg-red-400 rounded-lg" onClick={() => filterByCategory("beauty")}>Beauty</button>
+        <button className="max-sm:border border-box border-black hover:bg-red-400 rounded-lg" onClick={() => filterByCategory("fragrances")}>Fragrances</button>
+        <button className="max-sm:border border-box border-black hover:bg-red-400 rounded-lg" onClick={() => filterByCategory("furniture")}>Furniture</button>
+        <button className="max-sm:border border-box border-black hover:bg-red-400 rounded-lg" onClick={() => filterByCategory("groceries")}>Groceries</button>
+      </div>
+      <div className="  ">
+        <div className="grid grid-cols-2 sm:grid-cols-6 sm:gap-6  text-center ">
+          {passingData.map((item) => (
+            <div key={item.id} className="  flex flex-col m-3 h-[350px] border border-box border-red-300 rounded-lg">
+              {/* <Link to={`/Info/${item.id}/${item.title}`}> */}
+                <div className=" h-[50%] flex justify-center ">
+                  <img className="w-full border " src={item.images[0]} alt="product" />
+                </div>
+                <div className="h-[20%] text-xl font-semibold leading-6 ">
+                  <h1>{`${item.title}`}</h1>
+                </div>
+                <div className="h-[15%]  text-xl font-semibold  ">
+                  <p>{`$. ${item.price}`}</p>
+                </div>
+                <div className="h-[10%] ">
+                  <button className="border border-box border-red-300 hover:bg-red-400 text-xl p-3 font-bold rounded-lg" onClick={() => handleAddToCart(item)}>
+                    Add to cart
+                  </button>
+                </div>
+              {/* </Link> */}
+            </div>
+          ))}
         </div>
-        <div className="  ">
-          <div className="grid grid-cols-5 gap-10  text-center ">
-            {passingData.map((item) => (
-              <div key={item.id} className=" flex flex-col justify-center h-[400px] m-2 border border-box border-red-300 leading-normal rounded-lg">
-                <Link to={`/Info/${item.id}/${item.title}`}>
-                  <div className="h-[200px] w-[100%]">
-                    <img className="max-h-full w-full" src={item.images[0]} alt="product" />
-                    </div>
-                  <div className="h-[20%] text-xl font-semibold"> 
-                    <h1>{`${item.title}`}</h1>
-                    </div>
-                  <div className="h-[10%]  text-xl font-semibold">
-                    <p>{`$. ${item.price}`}</p>
-                    </div>
-                  <div className="h-[10%] ">
-                    <button className="border border-box border-blue-400 p-3 font-semibold rounded-lg" onClick={() => handleAddToCart(item)}>
-                      Add to cart
-                    </button>
-                  </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
-        
-      
+      </div>
+
+
     </>
   );
 };
